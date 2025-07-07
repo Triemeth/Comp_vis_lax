41d514650f46447c72d1069b2aaea8b2dcd5de9a
Ethan Triem <EJTriem@outlook.com>
2025-07-07 15:14:49 -0500

    save before running first time

--- base_yolo_model.py
+++ base_yolo_model.py
@@ -1,4 +1,10 @@
-import ultralytics
-ultralytics.checks()
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
 
+model = YOLO("yolo11n.pt")
+
+results = model.train(data = "lax.yml", epochs = 100, imgsz=640)
+
+metrics = model.val()
+metrics.box.maps
 #py -3.12 base_yolo_model.py
