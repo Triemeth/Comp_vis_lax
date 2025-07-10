18d000cdd12773bb754e471a3ffa00b4c3893d71
Ethan Triem <EJTriem@outlook.com>
2025-07-09 23:15:30 -0500

    organizing and simple fixes

--- gpu_checks.py
+++
@@ -1,9 +0,0 @@
-import torch
-from ultralytics.utils.torch_utils import select_device
-
-print("Selected device:", select_device('0'))
-
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU found")
-
-#py -3.12 gpu_checks.py
